f2bab6d8e31160f7266f36f44ec2e18d28bda8db
Megan Kurz <meganruthkurz@gmail.com>
2016-03-03 14:09:20 -0800

    Created Search Address, added geocode to mapGen, added search to dbConnect
    Added search Address function to let the user search for an address and
    give them feedback on the crime at that address if it exists/has a
    history with crime. Changed dbConnect to include the function to search
    the database. Added geocoder in mapGen.

--- fetchData.js
+++ fetchData.js
@@ -1,35 +1,35 @@
-<<<<<<< HEAD
 $(document).ready(function() {
-    $('#year').change(function () {
-        //get the value of the selection within the Year selections
-        var year = $('#year option:selected').val();
-            $.ajax({
-                type: 'GET',
-                url: 'http://localhost:8888/yearQuery/' + year,
-                useDefaultHxrHeader: false,
-                dataType: 'json',
-                success: function () {
-                    console.log("Success");
-                },
-                error: function (data) {
-                    console.log("Error.", data);
-                }
+	$('#year').change(function() {
+		// get the value of the selection within the Year selections
+		var year = $('#year option:selected').val();
+		$.ajax({
+			type : 'GET',
+			url : 'http://localhost:8888/yearQuery/' + year,
+			useDefaultHxrHeader : false,
+			dataType : 'json',
+			success : function() {
+				console.log("Success");
+			},
+			error : function(data) {
+				console.log("Error.", data);
+			}
 
-            });
-=======
-$(document).ready(function(){
-    $('#year').click(function() {
-        $.ajax({
-            type: 'GET',
-            url: 'http://localhost:8888/',
-            dataType: 'json',
-            success: function () {
+		});
 
-            },
-            error: function(data){
-                console.log("Error.", data);
-            }
-        });
->>>>>>> master
-    });
+		$(document).ready(function() {
+			$('#year').click(function() {
+				$.ajax({
+					type : 'GET',
+					url : 'http://localhost:8888/',
+					dataType : 'json',
+					success : function() {
+
+					},
+					error : function(data) {
+						console.log("Error.", data);
+					}
+				});
+			});
+		});
+	});
 });
