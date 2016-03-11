a1579581c05e13f5ab7b14587dd0fc88d3f98a35
lokidoki2 <schroener1@gmail.com>
2016-03-11 11:42:36 -0800

    searchAddress function now works

--- searchAddress.js
+++ searchAddress.js
@@ -1,4 +1,4 @@
-var rows;
+var geocoder = new google.maps.Geocoder();
 
 function searchAddress() {
     var address = $('#addressSearch').val();
@@ -14,7 +14,7 @@ function searchAddress() {
         dataType: 'json',
         useDefaultHxrHeader: false,
         success: function (data) {
-            rows = data;
+            var rows = data;
             rows = JSON.stringify(rows);
         },
         error: function (data) {
@@ -24,35 +24,34 @@ function searchAddress() {
         // send a pop up to let the user know
     });
     console.log(rows);
-}
-
-var addressObj = JSON.parse(rows);
-var infoString = '<div id="crimeInfo"><p>';
-for (i = 0; i < addressObj.length(); i++) {
-    infoString += addressObj[i].TYPE + " " + addressObj[i].YEAR + " "
-        + addressObj[i].MONTH + " " + addressObj[i].HUNDRED_BLOCK + " "
-        + addressObj[i].N_HOOD + "<br>";
-}
-
-geocoder.geocode({ 'address': address}, function (results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-        var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {center: (results[0].geometry.location), zoom: 15});
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-        marker.setMap(map);
-    } else {
-        alert('Geocode was not successful for the following reason: '
-            + status);
+    var addressObj = JSON.parse(rows);
+    var infoString = '<div id="crimeInfo"><p>';
+    for (i = 0; i < addressObj.length(); i++) {
+        infoString += addressObj[i].TYPE + " " + addressObj[i].YEAR + " "
+            + addressObj[i].MONTH + " " + addressObj[i].HUNDRED_BLOCK + " "
+            + addressObj[i].N_HOOD + "<br>";
     }
-    // use the geocode to find the lat and long variables and create a marker
-    // with the information about the crime at that address
-});
+
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            var mapDiv = document.getElementById('map');
+            var map = new google.maps.Map(mapDiv, {center: (results[0].geometry.location), zoom: 15});
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+            marker.setMap(map);
+        } else {
+            alert('Geocode was not successful for the following reason: '
+                + status);
+        }
+        // use the geocode to find the lat and long variables and create a marker
+        // with the information about the crime at that address
+    });
+}
