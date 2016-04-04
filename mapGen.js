b5d6a94b759fe33167c7bc9f398fc7f09af2a106
Ryan <kramer545@gmail.com>
2016-04-04 11:08:47 -0700

    removed bottom sidebar, cleaned up sidebar

--- mapGen.js
+++ mapGen.js
@@ -26,7 +26,7 @@ var cam = new Array(30);
 
 function initMap() {
     var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {center: {lat: 49.2827, lng: -123.1207}, zoom: 11});
+    var map = new google.maps.Map(mapDiv, {center: {lat: 49.26, lng: -123.1207}, zoom: 12});
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
